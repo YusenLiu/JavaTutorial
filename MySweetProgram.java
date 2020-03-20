4f74bf8b4019aa5639f2e1f0c6416413eb4a5022
Eason Liu <43985210+YusenLiu@users.noreply.github.com>
2020-03-20 14:55:22 -0500

    Update MySweetProgram.java

--- MySweetProgram.java
+++ MySweetProgram.java
@@ -1,10 +1,5 @@
 public class MySweetProgram {
     public static void main(String[] args) {
-//        String test = "asdf";
-//        boolean result;
-//
-//        result = test instanceof String;
-//        System.out.println(result);
         System.out.println("Hello" + args[0]);//needs to pass an argument to a program
         MySweetProgram x = new MySweetProgram(); // creating an object
         x.tacos();
@@ -14,3 +9,10 @@ public class MySweetProgram {
         System.out.println("Tacos");
     }
 }
+
+//        String test = "asdf";
+//        boolean result;
+//
+//        result = test instanceof String;
+//        System.out.println(result);
+//to be continued
