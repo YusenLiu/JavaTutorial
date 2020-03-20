f27856ec0d52f86ee26b94069665355009af047e
Eason Liu <43985210+YusenLiu@users.noreply.github.com>
2020-03-20 15:37:03 -0500

    Update MySweetProgram.java

--- MySweetProgram.java
+++ MySweetProgram.java
@@ -4,15 +4,23 @@ public class MySweetProgram {
         MySweetProgram x = new MySweetProgram(); // creating an object
         x.tacos();
     }
-
+    
     public void tacos(){
         System.out.println("Tacos");
     }
 }
 
-//        String test = "asdf";
-//        boolean result;
-//
-//        result = test instanceof String;
-//        System.out.println(result);
-//to be continued
+
+
+String test = "asdf";
+boolean result;
+result = test instanceof String;
+System.out.println(result);
+
+
+String str1 = new String("One way of initializing a string");
+String str2 = "Another way of initializing a string";
+int x = 5; //primitive type
+Integer y = 5; //class type - object (5 is autoboxed)
+
+to be continued
